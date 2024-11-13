f48d37f3291c3ae115fba6030cf34609daede7f7
trung300906 <taduytrung123@gmail.com>
2024-11-13 12:29:15 +0700

    challenger

--- challenger/pythoncode/fibornacci/finalfibor.py
+++ challenger/pythoncode/fibornacci/finalfibor.py
@@ -49,11 +49,17 @@ def parallel_fibonacci_positions(positions, modul):
 
     return results
 
-if __name__ == "__main__":
-    # Ví dụ vị trí cần tìm, bạn có thể thay đổi theo nhu cầu
-    positions = [10, 20, 10**8, 10**9, 10**10, 10**11, 10**2, 10**4]
-    modul = 10**7 - 1
+def MAIN(inputfile = "/run/media/trunglinux/linuxandwindows/code/CTDLGTVSOOP/challenger/pythoncode/inputfibornacci.txt"):
+    with open(inputfile, "r") as file:
+        data = file.readlines()
+    first_line = data[0].strip().split()
+    n = int(first_line[0])
+    modul = int(first_line[1])
+    positions = [int(line.strip()) for line in data[1:n+1]]
 
     results = parallel_fibonacci_positions(positions, modul)
-    for pos, fib in results:
-        print(f"F({pos}) % {modul} = {fib}")
+    return results, modul
+if __name__ == "__main__":
+    output , modul = MAIN("/run/media/trunglinux/linuxandwindows/code/CTDLGTVSOOP/challenger/pythoncode/inputfibornacci.txt")
+    #for pos, result in output:
+        #print(f"{pos} % {modul} = {result}")
