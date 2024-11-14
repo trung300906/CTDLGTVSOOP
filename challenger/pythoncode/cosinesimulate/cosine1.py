7f020fc6433602b7eabb1875f0b84bbfced597ee
trung300906 <taduytrung123@gmail.com>
2024-11-14 20:21:14 +0700

    parallel

--- challenger/pythoncode/cosinesimulate/cosine1.py
+++ challenger/pythoncode/cosinesimulate/cosine1.py
@@ -53,6 +53,6 @@ def MAIN(input_file):
 
 # Sử dụng hàm MAIN để tính toán ma trận cosine similarity
 if __name__ == "__main__":
-    similarity_matrix = MAIN("input.txt")
+    similarity_matrix = MAIN("/run/media/trunglinux/linuxandwindows/code/CTDLGTVSOOP/challenger/pythoncode/cosinesimulate/input.txt")
     for row in similarity_matrix:
         print(" ".join(f"{value:.4f}" for value in row))
