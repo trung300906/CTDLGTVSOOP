9230250c1b9dac13486bbbd163c2656e386367fc
trung300906 <taduytrung123@gmail.com>
2024-12-08 01:26:48 +0700

    project

--- codeforce/bai05.cpp
+++ codeforce/bai05.cpp
@@ -1,6 +1,22 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    int a[n];
+    int count = 0;
+    for (auto &i : a)
+    {
+        cin >> i;
+    }
+    int thre = a[k - 1];
+    for (auto &i : a)
+    {
+        if (i >= thre)
+            count++;
+    }
+    cout << count << endl;
+    return 0;
 }
