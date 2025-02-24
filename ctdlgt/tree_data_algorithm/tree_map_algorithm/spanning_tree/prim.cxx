0594528e264e545b306dba73c4c01945d030a4dd
trung300906 <taduytrung123@gmail.com>
2025-02-24 11:58:55 +0700

    prim.cxx

--- ctdlgt/tree_data_algorithm/tree_map_algorithm/spanning_tree/prim.cxx
+++ ctdlgt/tree_data_algorithm/tree_map_algorithm/spanning_tree/prim.cxx
@@ -1,12 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct compare
+{
+    bool operator()(pair<int, int> &a, pair<int, int> &b)
+    {
+        if (a.second == b.second)
+            return a.first > b.first;
+        return a.second > b.second;
+    }
+};
 class prim
 {
 private:
-    vector<vector<int>> maps;
-    priority_queue<pair<int, bool>, vector<pair<int, bool>>, greater<pair<int, bool>>> pq; // first is distance and second is boolean
+    vector<vector<int>> maps;                                                                                            // maps weight round for node e
+    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq; // first is dis and second is root to node
+    vector<bool> mark_node;                                                                                              // for mark node visited
     int node;
+    vector<int> ans[INT_MAX];
 
 public:
     prim()
@@ -22,7 +33,28 @@ public:
                 temp.push_back(x);
             }
             maps.push_back(temp);
-            pq.push(make_pair(false, INT_MAX));
+            mark_node.push_back(false);
+        }
+    }
+    void make_run()
+    {
+        pq.push(make_pair(0, make_pair(0, 0))); // dis 0 with node 0 to 0
+        while (!pq.empty() && any_of(mark_node.begin(), mark_node.end(), [](const bool &since)
+                                     { return !since; }))
+        {
+            int root = pq.top().second.first;
+            int dis = pq.top().second.second;
+            if (dis != root)
+            {
+                ans[root].push_back(dis);
+                ans[dis].push_back(root);
+            }
+            mark_node[root] = true;
+            pq.pop();
+            for (int itr = 0; itr < maps[root].size(); ++itr)
+            {
+                pq.push({maps[root][itr], {root, itr}});
+            }
         }
     }
 };
