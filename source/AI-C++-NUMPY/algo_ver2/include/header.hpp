f077b8da09b2f96a7959cdeac6ca1deb779c4c35
trung300906 <taduytrung123@gmail.com>
2025-03-13 23:13:33 +0700

    header

--- source/AI-C++-NUMPY/algo_ver2/include/header.hpp
+++ source/AI-C++-NUMPY/algo_ver2/include/header.hpp
@@ -7,8 +7,33 @@
 #include <functional>
 #include <initializer_list>
 #include <string>
-#include <immintrin.h> // AVX2
 #include <algorithm>
 #include <execution>
+
+// multi-thread
+#include <thread>
+#include <mutex>
+#include <atomic>
+#include <condition_variable>
+#include <future>
+
+// memory
+#include <memory>      // smart pointers (unique_ptr, shared_ptr)
+#include <cstdlib>     // malloc, free, aligned_alloc
+#include <new>         // placement new
+#include <mm_malloc.h> // _mm_malloc, _mm_free
+
+// AVX
+#include <immintrin.h> // AVX2
+#include <xmmintrin.h> // SSE
+#include <emmintrin.h> // SSE2
+#include <pmmintrin.h> // SSE3
+#include <smmintrin.h> // SSE4
+
+// MATH
+#include <cmath>   // C++ math functions
+#include <numeric> // Accumulate, inner_product, etc.
+#include <complex> // Complex numbers
+#include <random>  // Random number generation
 
 #endif
